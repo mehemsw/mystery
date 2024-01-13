3106966a33d79e865642beb0c4da527d4bd8d022
Matt Hemsworth <matthemsworth01@gmail.com>
2024-01-13 11:54:48 -0500

    changed variables

--- mystery.py
+++ mystery.py
@@ -1,30 +1,30 @@
 import re
 
-def my_func(x):
-    a = x.split(" ")
-    y = []
-    for x in a:
-        x2 = re.sub("[.!?]", "", x)
-        y.append(x2)
-    z = {}
-    for stuff in y:
-        if len(stuff) in z: 
-            z[len(stuff)] = z[len(stuff)] + [stuff]
+def make_dict_with_odd_length_words(input_string):
+    split_word_list = input_string.split(" ")
+    cleaned_list = []
+    for word in split_word_list:
+        clean_word = re.sub("[.!?]", "", word)
+        cleaned_list.append(clean_word)
+    temp_dict = {}
+    for word in cleaned_list:
+        if len(word) in temp_dict: 
+            temp_dict[len(word)] = temp_dict[len(word)] + [word]
         else:
-            z[len(stuff)]=[stuff]
-    a = {}
-    for y in z:
-        for x in z[y]:
-            if len(x) % 2 == 1:
-                a[y] = z[y]
+            temp_dict[len(word)]=[word]
+    final_dict = {}
+    for word_length in temp_dict:
+        for word in temp_dict[word_length]:
+            if len(word) % 2 == 1:
+                final_dict[word_length] = temp_dict[word_length]
         else:
             continue
-    return a
+    return final_dict
 
 
 
 
 # Tests
-assert(my_func("This is a sentence. And yet another one!") == {1: ['a'], 3: ['And', 'yet', 'one'], 7: ['another']})
-assert(my_func("Miscollated alphabetic superimposition") == {11: ['Miscollated'], 15: ['superimposition']})
-print(my_func("a a a a bb bb bb ccc ccc") == {1: ['a', 'a', 'a', 'a'], 3: ['ccc', 'ccc']})
+assert(make_dict_with_odd_length_words("This is a sentence. And yet another one!") == {1: ['a'], 3: ['And', 'yet', 'one'], 7: ['another']})
+assert(make_dict_with_odd_length_words("Miscollated alphabetic superimposition") == {11: ['Miscollated'], 15: ['superimposition']})
+assert(make_dict_with_odd_length_words("a a a a bb bb bb ccc ccc") == {1: ['a', 'a', 'a', 'a'], 3: ['ccc', 'ccc']})
