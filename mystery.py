ff036e9898f0786bac6ef8cfefd01112bd07ee44
sfuent1 <sfuent1@gmail.com>
2024-01-13 13:07:15 -0500

    divided main function into multiple functions

--- mystery.py
+++ mystery.py
@@ -18,18 +18,23 @@ def group_words_length(clean_words):
     return word_lengths
 
 def odd_length_words(word_lengths):
-    odd_words_list = {}
+    odd_words_dict = {}
     for clean_words in word_lengths:
         for x in word_lengths[clean_words]:
-            if len(str) % 2 == 1:
+            if len(x) % 2 == 1:
                 word_lengths[clean_words] = word_lengths[clean_words]
         else:
             continue
-    return odd_words_list
+    return odd_words_dict
+
 
 
 
 # Tests
-assert(clean_words_list("This is a sentence. And yet another one!") == {1: ['a'], 3: ['And', 'yet', 'one'], 7: ['another']})
-assert(clean_words_list("Miscollated alphabetic superimposition") == {11: ['Miscollated'], 15: ['superimposition']})
-print(clean_words_list("a a a a bb bb bb ccc ccc") == {1: ['a', 'a', 'a', 'a'], 3: ['ccc', 'ccc']})
+# assert(clean_words_list("This is a sentence. And yet another one!") == {1: ['a'], 3: ['And', 'yet', 'one'], 7: ['another']})
+# assert(clean_words_list("Miscollated alphabetic superimposition") == {11: ['Miscollated'], 15: ['superimposition']})
+# print(clean_words_list("a a a a bb bb bb ccc ccc") == {1: ['a', 'a', 'a', 'a'], 3: ['ccc', 'ccc']})
+
+print(clean_words_list("This is a sentence. And yet another one!"))
+print(group_words_length(clean_words_list("This is a sentence. And yet another one!")))
+print(odd_length_words(group_words_length(clean_words_list("This is a sentence. And yet another one!"))))
